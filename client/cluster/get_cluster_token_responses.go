820149eed3de55dbf79c5597fa454b9a01b9e42d
Signadot <git@signadot.com>
2022-09-15 14:09:58 +0000

    swagger generation

--- client/cluster/get_cluster_token_responses.go
+++ client/cluster/get_cluster_token_responses.go
@@ -47,9 +47,39 @@ type GetClusterTokenOK struct {
 	Payload *models.ClusterToken
 }
 
+// IsSuccess returns true when this get cluster token o k response has a 2xx status code
+func (o *GetClusterTokenOK) IsSuccess() bool {
+	return true
+}
+
+// IsRedirect returns true when this get cluster token o k response has a 3xx status code
+func (o *GetClusterTokenOK) IsRedirect() bool {
+	return false
+}
+
+// IsClientError returns true when this get cluster token o k response has a 4xx status code
+func (o *GetClusterTokenOK) IsClientError() bool {
+	return false
+}
+
+// IsServerError returns true when this get cluster token o k response has a 5xx status code
+func (o *GetClusterTokenOK) IsServerError() bool {
+	return false
+}
+
+// IsCode returns true when this get cluster token o k response a status code equal to that given
+func (o *GetClusterTokenOK) IsCode(code int) bool {
+	return code == 200
+}
+
 func (o *GetClusterTokenOK) Error() string {
 	return fmt.Sprintf("[GET /orgs/{orgName}/clusters/{clusterName}/tokens/{tokenId}][%d] getClusterTokenOK  %+v", 200, o.Payload)
 }
+
+func (o *GetClusterTokenOK) String() string {
+	return fmt.Sprintf("[GET /orgs/{orgName}/clusters/{clusterName}/tokens/{tokenId}][%d] getClusterTokenOK  %+v", 200, o.Payload)
+}
+
 func (o *GetClusterTokenOK) GetPayload() *models.ClusterToken {
 	return o.Payload
 }
