95512b6d925f468c456e2efd4e413113cb3c675f
Signadot <git@signadot.com>
2023-03-10 01:04:21 +0000

    swagger generation

--- client/route_groups/get_routegroup_parameters.go
+++ client/route_groups/get_routegroup_parameters.go
@@ -52,12 +52,10 @@ func NewGetRoutegroupParamsWithHTTPClient(client *http.Client) *GetRoutegroupPar
 	}
 }
 
-/*
-GetRoutegroupParams contains all the parameters to send to the API endpoint
+/* GetRoutegroupParams contains all the parameters to send to the API endpoint
+   for the get routegroup operation.
 
-	for the get routegroup operation.
-
-	Typically these are written to a http.Request.
+   Typically these are written to a http.Request.
 */
 type GetRoutegroupParams struct {
 
