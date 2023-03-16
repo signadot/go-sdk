05c127533f83ce67524b965b25b79c293a14a5bf
Signadot <git@signadot.com>
2023-03-16 10:07:39 +0000

    swagger generation

--- client/resource_plugins/delete_resource_plugin_parameters.go
+++ client/resource_plugins/delete_resource_plugin_parameters.go
@@ -52,12 +52,10 @@ func NewDeleteResourcePluginParamsWithHTTPClient(client *http.Client) *DeleteRes
 	}
 }
 
-/*
-DeleteResourcePluginParams contains all the parameters to send to the API endpoint
+/* DeleteResourcePluginParams contains all the parameters to send to the API endpoint
+   for the delete resource plugin operation.
 
-	for the delete resource plugin operation.
-
-	Typically these are written to a http.Request.
+   Typically these are written to a http.Request.
 */
 type DeleteResourcePluginParams struct {
 
