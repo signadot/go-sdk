546c084cabb47a4d159bf0a26fdb7943e8c3edbb
Signadot <git@signadot.com>
2025-04-29 18:55:10 +0000

    swagger generation

--- client/signadot_api_client.go
+++ client/signadot_api_client.go
@@ -11,6 +11,8 @@ import (
 	"github.com/go-openapi/strfmt"
 
 	"github.com/signadot/go-sdk/client/artifacts"
+	"github.com/signadot/go-sdk/client/assistants"
+	"github.com/signadot/go-sdk/client/auth"
 	"github.com/signadot/go-sdk/client/cluster"
 	"github.com/signadot/go-sdk/client/job_logs"
 	"github.com/signadot/go-sdk/client/jobs"
@@ -65,6 +67,8 @@ func New(transport runtime.ClientTransport, formats strfmt.Registry) *SignadotAP
 	cli := new(SignadotAPI)
 	cli.Transport = transport
 	cli.Artifacts = artifacts.New(transport, formats)
+	cli.Assistants = assistants.New(transport, formats)
+	cli.Auth = auth.New(transport, formats)
 	cli.Cluster = cluster.New(transport, formats)
 	cli.JobLogs = job_logs.New(transport, formats)
 	cli.Jobs = jobs.New(transport, formats)
@@ -120,6 +124,10 @@ func (cfg *TransportConfig) WithSchemes(schemes []string) *TransportConfig {
 type SignadotAPI struct {
 	Artifacts artifacts.ClientService
 
+	Assistants assistants.ClientService
+
+	Auth auth.ClientService
+
 	Cluster cluster.ClientService
 
 	JobLogs job_logs.ClientService
@@ -145,6 +153,8 @@ type SignadotAPI struct {
 func (c *SignadotAPI) SetTransport(transport runtime.ClientTransport) {
 	c.Transport = transport
 	c.Artifacts.SetTransport(transport)
+	c.Assistants.SetTransport(transport)
+	c.Auth.SetTransport(transport)
 	c.Cluster.SetTransport(transport)
 	c.JobLogs.SetTransport(transport)
 	c.Jobs.SetTransport(transport)
