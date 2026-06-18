86c36bf3bfefd0995eab516070975dc8288cbe58
Signadot <git@signadot.com>
2026-06-18 13:51:59 +0000

    swagger generation

--- client/meta/meta_client.go
+++ client/meta/meta_client.go
@@ -53,56 +53,9 @@ type ClientOption func(*runtime.ClientOperation)
 
 // ClientService is the interface for Client methods
 type ClientService interface {
-	MetaOpenapi(params *MetaOpenapiParams, opts ...ClientOption) (*MetaOpenapiOK, error)
-
 	MetaPlans(params *MetaPlansParams, opts ...ClientOption) (*MetaPlansOK, error)
 
 	SetTransport(transport runtime.ClientTransport)
-}
-
-/*
-MetaOpenapi documenteds open API spec
-
-Returns the customer-facing OpenAPI (Swagger 2.0) document for
-*/
-func (a *Client) MetaOpenapi(params *MetaOpenapiParams, opts ...ClientOption) (*MetaOpenapiOK, error) {
-	// NOTE: parameters are not validated before sending
-	if params == nil {
-		params = NewMetaOpenapiParams()
-	}
-	op := &runtime.ClientOperation{
-		ID:                 "meta-openapi",
-		Method:             "GET",
-		PathPattern:        "/meta/openapi.json",
-		ProducesMediaTypes: []string{"application/json"},
-		ConsumesMediaTypes: []string{"application/json"},
-		Schemes:            []string{"https"},
-		Params:             params,
-		Reader:             &MetaOpenapiReader{formats: a.formats},
-		Context:            params.Context,
-		Client:             params.HTTPClient,
-	}
-	for _, opt := range opts {
-		opt(op)
-	}
-	result, err := a.transport.Submit(op)
-	if err != nil {
-		return nil, err
-	}
-
-	// only one success response has to be checked
-	success, ok := result.(*MetaOpenapiOK)
-	if ok {
-		return success, nil
-	}
-
-	// unexpected success response.
-
-	// no default response is defined.
-	//
-	// safeguard: normally, in the absence of a default response, unknown success responses return an error above: so this is a codegen issue
-	msg := fmt.Sprintf("unexpected success response for meta-openapi: API contract not enforced by server. Client expected to get an error, but got: %T", result)
-	panic(msg)
 }
 
 /*
